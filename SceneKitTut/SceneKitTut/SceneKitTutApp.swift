67d26208e83f84c787d8e7dee88e4ea89e99862e
sahil-sahu-dev <80636783+sahil-sahu-dev@users.noreply.github.com>
2022-06-16 10:50:31 +0530

    Add files via upload

---
+++ SceneKitTut/SceneKitTut/SceneKitTutApp.swift
@@ -0,0 +1,17 @@
+//
+//  SceneKitTutApp.swift
+//  SceneKitTut
+//
+//  Created by Sahil Sahu on 15/06/22.
+//
+
+import SwiftUI
+
+@main
+struct SceneKitTutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
